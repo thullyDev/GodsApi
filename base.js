ae00dbe85ae1077686a8af731f3005520d1d5106
thullyDev <thully.developer@gmail.com>
2023-06-05 01:01:23 +0200

    finished manganato scraping

--- base.js
+++ base.js
@@ -1,53 +1,105 @@
-import express  from "express"
-import axios from 'axios'
-import cheerio from 'cheerio'
-import { ManganatoParser } from './resources/parser.js'
-import {
-	PORT,
-	USER_AGENT, 
-	SUCESSFUL, 
-	print 
-} from './resources/utilities.js'
+import express from 'express';
+import axios from 'axios';
+import cheerio from 'cheerio';
+import { ManganatoParser } from './resources/parser.js';
+import { PORT, USER_AGENT, SUCESSFUL, print } from './resources/utilities.js';
 
 const app = express();
 const mnt_parser = new ManganatoParser();
 
 app.use(express.json());
 
-app.listen(PORT, function() {
-	print("GodsScraper-v1 ===> http://localhost:"+PORT)
-})
+app.listen(PORT, function () {
+  print('GodsScraper-v1.0.0 ===> http://localhost:' + PORT);
+});
 
-app.get("/", function (req, res) {
-	res.status(SUCESSFUL).send(" Welcome to GodsScraper-v1\n Credit to https://github.com/thullyDev for this manga/hentai/anime Api Scraper")
-})
+app.get('/', function (req, res) {
+  res
+    .status(SUCESSFUL)
+    .send(
+      ' Welcome to GodsScraper-v1\n Credit to https://github.com/thullyDev for this manga/hentai/anime Api Scraper'
+    );
+});
 
-app.get("/read/:manga_id/:chapter", async function(req, res) {
-	const manga_id = req.params.manga_id
-	const chapter = req.params.chapter
-	const site = req.query.s
-	let data = {}
-	await mnt_parser.get_panels(manga_id, chapter, site,  function(results) {
-		data = results
-	})
-	res.status(data.status_code).send(data)	 
-})
+app.get('/read/:manga_id/:chapter', async function (req, res) {
+  const manga_id = req.params.manga_id;
+  const chapter = req.params.chapter;
+  const site = req.query.s;
+  let data = {};
+  await mnt_parser.get_panels(manga_id, chapter, site, function (results) {
+    data = results;
+  });
+  res.status(data.status_code).send(data);
+});
 
-app.get("/panel/:src", async function(req, res) {
-	const source = req.params.src
-	let data = {}
-	await mnt_parser.get_panel(source, (results) => {
-		data = results
-	})
-	res.status(data.status_code).send(data) 
-})
+app.get('/panel/:src', async function (req, res) {
+  const source = req.params.src;
+  let data = {};
+  await mnt_parser.get_panel(source, (results) => {
+    data = results;
+  });
+  res.status(data.status_code).send(data);
+});
 
+app.get('/manga/:manga_id/', async function (req, res) {
+  const manga_id = req.params.manga_id;
+  let data = {};
+  await mnt_parser.get_manga_info(manga_id, (results) => {
+    data = results;
+  });
+  res.status(data.status_code).send(data);
+});
 
-app.get("/manga/:manga_id/", async function(req, res) {
-	const manga_id = req.params.manga_id
-	let data = {}
-	await mnt_parser.get_manga_info(manga_id, (results) => {
-		data = results
-	})
-	res.status(data.status_code).send(data)
-})
+app.get('/latest', async function (req, res) {
+  let data = {};
+  const status = req.query.status;
+  await mnt_parser.get_latest(status, (results) => {
+    data = results;
+  });
+  res.status(data.status_code).send(data);
+});
+
+app.get('/newest', async function (req, res) {
+  let data = {};
+  const status = req.query.status;
+  await mnt_parser.get_newest(status, (results) => {
+    data = results;
+  });
+  res.status(data.status_code).send(data);
+});
+
+app.get('/alltimes', async function (req, res) {
+  let data = {};
+  const status = req.query.status;
+  await mnt_parser.get_alltimes(status, (results) => {
+    data = results;
+  });
+  res.status(data.status_code).send(data);
+});
+
+app.get('/genres', async function (req, res) {
+  let data = {};
+  const cache = req.query.cache;
+  await mnt_parser.get_genres(cache, (results) => {
+    data = results;
+  });
+  res.status(data.status_code).send(data);
+});
+
+app.get('/filter/:genre', async function (req, res) {
+  let data = {};
+  const status = req.query.status;
+  const genre = req.params.genre;
+  await mnt_parser.filter(genre, status, (results) => {
+    data = results;
+  });
+  res.status(data.status_code).send(data);
+});
+
+app.get('/top_mangas', async function (req, res) {
+  let data = {};
+  await mnt_parser.get_top_mangas((results) => {
+    data = results;
+  });
+  res.status(data.status_code).send(data);
+});
--- resources/utilities.js
+++ resources/utilities.js
@@ -1,43 +1,52 @@
-const PORT = 8080 
-const USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36'
-const SUCESSFUL = 200
-const NOT_FOUND = 404
-const FORBIDEEN = 403
-const CRASH = 503
+const PORT = 8080;
+const USER_AGENT =
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36';
+const SUCESSFUL = 200;
+const NOT_FOUND = 404;
+const FORBIDEEN = 403;
+const CRASH = 503;
+const SUCESSFUL_MSG = 'sucessful';
+const NOT_FOUND_MSG = 'not found';
+const FORBIDEEN_MSG = 'request forbidden';
+const CRASH_MSG = 'unexpected issue';
 const HEADERS = {
-    "accept": "image/avif,image/webp,image/apng,image/svg+xml,image/,/*;q=0.8",
-    "accept-language": "en-US,en;q=0.9",
-    "sec-ch-ua": '"Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": "Windows",
-    "sec-fetch-dest": "image",
-    "sec-fetch-mode": "no-cors",
-    "sec-fetch-site": "cross-site",
-    "Referrer-Policy": "strict-origin-when-cross-origin"
-}
-const mangangato_host = "https://manganato.com"
-const mangapill_host = "https://www.mangapill.com"
-const mangafoxfull_host = "https://mangafoxfull.com"
-const mangafoxfun_host = "https://mangafox.fun"
-const nhentai_host = "https://nhentai.net"
-const imhentai_host = "https://imhentai.xxx"
-const kisscartoon_host = "https://www1.kisscartoon.online"
-const print = (msg) => console.log(msg)
+  accept: 'image/avif,image/webp,image/apng,image/svg+xml,image/,/*;q=0.8',
+  'accept-language': 'en-US,en;q=0.9',
+  'sec-ch-ua': '"Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
+  'sec-ch-ua-mobile': '?0',
+  'sec-ch-ua-platform': 'Windows',
+  'sec-fetch-dest': 'image',
+  'sec-fetch-mode': 'no-cors',
+  'sec-fetch-site': 'cross-site',
+  'Referrer-Policy': 'strict-origin-when-cross-origin',
+};
+const mangangato_host = 'https://manganato.com';
+const mangapill_host = 'https://www.mangapill.com';
+const mangafoxfull_host = 'https://mangafoxfull.com';
+const mangafoxfun_host = 'https://mangafox.fun';
+const nhentai_host = 'https://nhentai.net';
+const imhentai_host = 'https://imhentai.xxx';
+const kisscartoon_host = 'https://www1.kisscartoon.online';
+const print = (msg) => console.log(msg);
 
 export {
-	PORT, 
-	USER_AGENT,
-	HEADERS,
-	SUCESSFUL,
-	NOT_FOUND,
-	FORBIDEEN,
-	CRASH,
-	mangangato_host, 
-	mangapill_host,
-	mangafoxfull_host,
-	mangafoxfun_host, 
-	nhentai_host, 
-	imhentai_host, 
-	kisscartoon_host, 
-	print,
+  PORT,
+  USER_AGENT,
+  HEADERS,
+  SUCESSFUL,
+  NOT_FOUND,
+  FORBIDEEN,
+  CRASH,
+  SUCESSFUL_MSG,
+  NOT_FOUND_MSG,
+  FORBIDEEN_MSG,
+  CRASH_MSG,
+  mangangato_host,
+  mangapill_host,
+  mangafoxfull_host,
+  mangafoxfun_host,
+  nhentai_host,
+  imhentai_host,
+  kisscartoon_host,
+  print,
 };
